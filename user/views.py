802995d074c55c58cdfb78764a5d14d9577fe0ea
chenmin <chenmin@lls.sz.cn>
2020-07-22 14:57:08 +0800

    添加用户信息

--- user/views.py
+++ user/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from user.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=['get'])
+    def userinfo(self, request):
+        """
+        用户信息
+        :param: request.
+        :return: User
+        """
+        user = self.request.user
+        serializer = UserSerializer(user)
+        return Response({"data": serializer.data})

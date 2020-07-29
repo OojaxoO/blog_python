3be9a28d2e4f33ff19ca0741094ed9e82cb3feb0
chenmin <chenmin@lls.sz.cn>
2020-07-29 16:59:27 +0800

    添加用户详情返回用户是否为管理员

--- user/serializers.py
+++ user/serializers.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'is_staff']
+        fields = ['username', 'email', 'is_superuser']
